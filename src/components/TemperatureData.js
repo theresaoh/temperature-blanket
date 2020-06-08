3a1cd7a93ed1e159bbb5b75bf64d87313051f832
Theresa O'Hare <theresa.ohare@outlook.com>
2020-06-07 22:42:32 -0600

    getting daily temperature, returning average temperature for a given day

--- src/components/TemperatureData.js
+++ src/components/TemperatureData.js
@@ -1,13 +1,47 @@
 import React from 'react';
-// import Axios from 'axios';
+import axios from 'axios';
+import moment from 'moment';
 
+const apiKey = process.env.REACT_APP_API_KEY;
+const station = process.env.REACT_APP_WEATHER_STATION;
+const apiUrl = process.env.REACT_APP_API_URL;
 
 class TemperatureData extends React.Component {
   constructor(props) {
     super(props);
     this.state = { 
       prop: "What's up",
+      date: '',
     };
+  }
+
+  componentDidMount(){
+    let dateToday = moment().subtract(2, 'days').format('YYYY-MM-DD');
+    this.setState({
+      date: dateToday
+    }, this.getDailyWeatherData)
+  }
+
+  getDailyWeatherData(){
+    const url = `${apiUrl}?station=${station}&start=${this.state.date}&end=${this.state.date}&key=${apiKey}`;
+    axios.get(url)
+      .then((res) => {
+        this.calculateAverageTemp(res.data.data[0]);
+      })
+  }
+
+  calculateAverageTemp(data){
+    // format from c to f first:
+    const high = this.celsiusToFahrenheit(data.temperature_max);
+    const low = this.celsiusToFahrenheit(data.temperature_min);
+    // then get the average temperature for the day
+    let total = high + low;
+    let average = total / 2;
+    return average;
+  }
+
+  celsiusToFahrenheit(temp){
+    return (temp * (9/5) + 32);
   }
 
   render() {
