924e34a1483bb0b6fc9f83271c33c21a40af402f
Theresa O'Hare <theresa.ohare@outlook.com>
2020-07-05 21:51:00 -0600

    color ranges now in code, being displayed along with daily averages (rounded to two decimal points)

--- client/src/components/TemperatureData.js
+++ client/src/components/TemperatureData.js
@@ -8,11 +8,8 @@ const apiUrl = process.env.REACT_APP_API_URL;
 
 /*
 
-TODO: Do more shopping around for APIs
 TODO: Double-check values being returned as averages
 TODO: Write a test for logic
-TODO: Set color ranges
-TODO: Connect color ranges and averages
 TODO: Display it all in a way that doesn't suck
 TODO: God, so many things. Database? Ability to cross off finished rows?
 
@@ -22,7 +19,6 @@ class TemperatureData extends React.Component {
   constructor(props) {
     super(props);
     this.state = { 
-      prop: "What's up",
       date: '',
       isLoading: true,
       tempData: [],
@@ -36,20 +32,32 @@ class TemperatureData extends React.Component {
   async getDailyWeatherData(){
     let startingDate = '20200101';
     let arr = [];
-    for (let i = 0; i < 5; i++){
+    let endOfLoop = this.daysElapsedSinceStart(startingDate)
+    for (let i = 0; i < endOfLoop; i++){
       let initialPlus = moment(startingDate).add(i, 'days');
       let date = initialPlus.format('YYYY-MM-DD');
       const url = `${apiUrl}?station=${station}&start=${date}&end=${date}&key=${apiKey}`;
       const data = await axios.get(url);
       let result = this.calculateAverageTemp(data.data.data[0]);
+      console.log(`${i} ${result}`);
+      let color = this.determineColor(result);
       let formattedDate = initialPlus.format('ll');
-      let obj = {"date": formattedDate, "avg_temp": result};
+      let obj = {"date": formattedDate, "avg_temp": result, "color": color};
       arr.push(obj);
     }
     this.setState({
       tempData: arr,
       isLoading: false
     })
+  }
+
+  daysElapsedSinceStart(startingDate){
+    var now = moment(new Date()); //todays date
+    var end = moment(startingDate); // another date
+    var duration = moment.duration(now.diff(end));
+    var days = duration.asDays();
+    let rounded = Math.floor(days);
+    return rounded
   }
 
   calculateAverageTemp(data){
@@ -59,22 +67,92 @@ class TemperatureData extends React.Component {
     // then get the average temperature for the day
     let total = high + low;
     let average = total / 2;
-    return average;
+    return average.toFixed(2);
   }
 
   celsiusToFahrenheit(temp){
     return (temp * (9/5) + 32);
   }
 
+  determineColor(temp){
+    let color;
+    switch (true) {
+      case (temp <= 29.9):
+        color = "Navy";
+        break;
+      case (temp > 29.9 && temp <= 34.5):
+        color = "Colonial Blue";
+        break;
+      case (temp > 34.5 && temp <= 39.1):
+        color = "Sky Blue";
+        break;
+      case (temp > 39.1 && temp <= 43.7):
+        color = "Silver Blue";
+        break;
+      case (temp > 43.7 && temp <= 48.3):
+        color = "Dusty Blue";
+        break;
+      case (temp > 48.3 && temp <= 52.9):
+        color = "Peacock";
+        break;
+      case (temp > 52.9 && temp <= 57.5):
+        color = "Olive";
+        break;
+      case (temp > 57.5 && temp <= 62.1):
+        color = "Dusty Green";
+        break;
+      case (temp > 62.1 && temp <= 66.7):
+        color = "Mustard";
+        break;
+      case (temp > 66.7 && temp <= 71.3):
+        color = "Rust";
+        break;
+      case (temp > 71.3 && temp <= 75.9):
+        color = "Terracotta";
+        break;
+      case (temp > 75.9):
+        color = "Cranberry";
+        break;
+      default:
+        color = "Couldn't calculate"
+    }
+    return color
+  }
+
   renderData(){
     const { tempData } = this.state;
-    for (let i = 0; i < 30; i++){
-      return (
-        tempData.map((item) => (
-          <div>{item.date}: {item.avg_temp}</div>
-        ))
-      )
-    }
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>
+              Date
+            </th>
+            <th>
+              Average Temp
+            </th>
+            <th>
+              Corresponding Color
+            </th>
+          </tr>
+        </thead>
+      {tempData.map((item, index) => (
+        <tbody key={index}>
+          <tr>
+            <td>
+              {item.date}
+            </td>
+            <td>
+              {item.avg_temp}
+            </td>
+            <td>
+              {item.color}
+            </td>
+          </tr>
+        </tbody>
+      ))}
+      </table>
+    )
   }
 
   render() {
